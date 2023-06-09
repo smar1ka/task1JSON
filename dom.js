865a75c675aba1cbf88839c68de0b06a72ab549e
Smarika Sijapati <sijapatismarika@gmail.com>
2023-06-09 14:42:30 +0545

    task1 added

---
+++ dom.js
@@ -0,0 +1,70 @@
+ //FORM ALIGNMENT - HORIZONTAL
+ var container=document.createElement('div');
+ container.style.height='100px';
+ container.style.width='500px';
+ container.style.display = 'flex';
+ container.style.justifyContent='space-between';
+ 
+ //BUTTON 
+ var myButton = document.createElement('button');
+ myButton.style.backgroundColor = 'red';
+ myButton.style.width="150px";
+ myButton.style.height="50px";
+ myButton.style.border="none";
+ myButton.click();
+ 
+//TEXTBOX
+var textbox = document.createElement("input");
+textbox.setAttribute("type","text");
+textbox.style.height="45px";
+textbox.style.width="45px";
+//textbox.setAttribute("placeholder","type here");
+
+//LABEL
+var label=document.createElement('label');
+label.textContent='Username';
+label.style.color='red';
+//label.style.textAlign='right';
+label.style.fontSize='40px';
+container.appendChild(myButton);
+container.appendChild(textbox);
+container.appendChild(label);
+
+document.body.appendChild(container);
+
+//FORM ALIGNMENT - VERTICAL
+var Container = document.createElement('div');
+Container.style.height='300px';
+Container.style.display='flex';
+Container.style.flexDirection='column';
+Container.style.justifyContent='space-between'
+//BUTTON
+var button=document.createElement('button');
+button.style.backgroundColor = 'yellow';
+button.style.width='150px';
+button.style.height="50px";
+button.style.border="none";
+button.click();
+//TEXTBOX
+var Textbox=document.createElement('input');
+Textbox.type='text';
+Textbox.style.width='30px';
+Textbox.style.height='30px';
+
+//LABEL
+var Label=document.createElement('label');
+Label.textContent='Password';
+Label.style.color='red';
+Label.style.fontSize='30px'
+
+Container.appendChild(button);
+Container.appendChild(Textbox);
+Container.appendChild(Label);
+
+document.body.appendChild(Container);
+
+
+ 
+
+
+
