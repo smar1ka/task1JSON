20482a7646f37b40d0943df92fe0c57fd8035c04
Smarika Sijapati <sijapatismarika@gmail.com>
2023-06-12 22:20:52 +0545

    modified

--- dom.js
+++ dom.js
@@ -1,70 +1,46 @@
- //FORM ALIGNMENT - HORIZONTAL
- var container=document.createElement('div');
- container.style.height='100px';
- container.style.width='500px';
- container.style.display = 'flex';
- container.style.justifyContent='space-between';
- 
- //BUTTON 
- var myButton = document.createElement('button');
- myButton.style.backgroundColor = 'red';
- myButton.style.width="150px";
- myButton.style.height="50px";
- myButton.style.border="none";
- myButton.click();
- 
-//TEXTBOX
-var textbox = document.createElement("input");
-textbox.setAttribute("type","text");
-textbox.style.height="45px";
-textbox.style.width="45px";
-//textbox.setAttribute("placeholder","type here");
+	fetch('ui.json')
+  .then(response => response.json())
+  .then(data => {
+    // Access the JSON data and create HTML elements
+    data.forEach(form => {
 
-//LABEL
-var label=document.createElement('label');
-label.textContent='Username';
-label.style.color='red';
-//label.style.textAlign='right';
-label.style.fontSize='40px';
-container.appendChild(myButton);
-container.appendChild(textbox);
-container.appendChild(label);
+     
+      var formContainer = document.createElement('div');
+      
+      // Set form alignment
+      formContainer.style.display =  'flex' ;
 
-document.body.appendChild(container);
+      // formContainer.style.flexDirection =  'column' ;
+      formContainer.style.flexDirection = form.form_alignment === 'vertical' ? 'column' : 'row';
+      
+      form.form_elements.forEach(element => {
+        const elementContainer = document.createElement('div');
+        
+        if (element.type === 'button') {
+          const button = document.createElement('button');
+          button.textContent = 'Button';
+          button.style.backgroundColor = element.color;
+          elementContainer.appendChild(button);
+        } else if (element.type === 'textBox') {
+          const textBox = document.createElement('input');
+          textBox.type = 'text';
+          textBox.size = element.size;
+          elementContainer.appendChild(textBox);
+        } else if (element.type === 'label') {
+          const label = document.createElement('label');
+          label.textContent = element.content;
+          label.style.color = element.color;
+          elementContainer.appendChild(label);
+        }
+        
+        formContainer.appendChild(elementContainer);
+      });
+      
+      document.body.appendChild(formContainer);
+    });
 
-//FORM ALIGNMENT - VERTICAL
-var Container = document.createElement('div');
-Container.style.height='300px';
-Container.style.display='flex';
-Container.style.flexDirection='column';
-Container.style.justifyContent='space-between'
-//BUTTON
-var button=document.createElement('button');
-button.style.backgroundColor = 'yellow';
-button.style.width='150px';
-button.style.height="50px";
-button.style.border="none";
-button.click();
-//TEXTBOX
-var Textbox=document.createElement('input');
-Textbox.type='text';
-Textbox.style.width='30px';
-Textbox.style.height='30px';
-
-//LABEL
-var Label=document.createElement('label');
-Label.textContent='Password';
-Label.style.color='red';
-Label.style.fontSize='30px'
-
-Container.appendChild(button);
-Container.appendChild(Textbox);
-Container.appendChild(Label);
-
-document.body.appendChild(Container);
-
-
- 
-
-
-
+    // console.log(data[0]);return ;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
